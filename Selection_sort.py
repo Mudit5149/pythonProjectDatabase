9e40fdeadb47628c6778a00d02fe9b0ef55b9155
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2022-06-20 15:40:51 +0530

    Add files via upload

---
+++ Selection_sort.py
@@ -0,0 +1,20 @@
+def sort(nums):
+
+    for i in range(5):
+        minpos = i
+        for j in range(i,6):
+            if nums[j] < nums[minpos]:
+                minpos = j
+
+
+        temp = nums[i]
+        nums[i] = nums[minpos]
+        nums[minpos] = temp
+
+        print(nums)
+
+
+nums = [4, 3, 7, 8, 6, 2]
+sort(nums)
+
+print(nums)
