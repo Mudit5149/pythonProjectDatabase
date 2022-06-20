3b0e97decd65b6e70e7b2a776f5a9223043b9b0b
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2022-06-20 15:47:03 +0530

    Add files via upload

---
+++ Bubble_Sort.py
@@ -0,0 +1,14 @@
+def sort(nums):
+
+    for i in range(len(nums)-1,0,-1):
+        for j in range(i):
+            if nums[j]>nums[j+1]:
+                temp = nums[j]
+                nums[j] = nums[j+1]
+                nums[j+1] = temp
+
+
+nums = [5, 9, 4, 3, 2,6]
+sort(nums)
+
+print(nums)
