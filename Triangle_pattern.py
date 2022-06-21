8900f42d9066c2cae5a28d391d131cb351244874
Mudit5149 <86524837+Mudit5149@users.noreply.github.com>
2022-06-21 12:01:39 +0530

    Add files via upload

---
+++ Triangle_pattern.py
@@ -0,0 +1,24 @@
+n=7
+for i in range(n):
+    for j in range(i+1):
+        print('*', end='')
+    print()
+
+
+    ############-----------------------------------------------------------#############
+# Printing pattern of star in Deacreasing order
+
+
+n=7
+for i in range(n):
+    for j in range(i ,n):
+        print('*', end='')
+    print()
+#------------------program of a Triangle in a reverse orde---------------------------------------
+n = 7
+for i in range(n):
+    for j in range(i, n):
+        print(" ", end='')
+    for j in range(i+1):
+        print('*',end='')
+     print()
